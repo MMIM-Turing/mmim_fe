b657e49b64efd2be012b11404a49f73196c01ecc
Will Medders <wmedders@gmail.com>
2022-06-03 17:20:52 -0400

    Feature test: landing index form submit sends user to results page

--- spec/landing/index_spec.rb
+++ spec/landing/index_spec.rb
@@ -8,5 +8,18 @@ RSpec.describe 'landing page' do
       expect(page).to have_field(:address_1)
       expect(page).to have_field(:address_2)
     end
+
+    it 'has a category field' do
+      visit '/'
+
+      expect(page).to have_field(:category)
+    end
+
+    it 'sends user to results page' do
+      visit '/'
+      click_button "Search"
+
+      expect(current_path).to eq('/results')
+    end
   end
 end
