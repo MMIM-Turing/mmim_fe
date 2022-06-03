0f4f91a2684da0ef8ef5a31df388127853ff88bf
Will Medders <wmedders@gmail.com>
2022-06-03 18:44:50 -0400

    Move landing spec folder into features

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,7 +64,6 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  config.include Capybara::DSL
 end
 
 Shoulda::Matchers.configure do |config|
