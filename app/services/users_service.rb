469031c951b7e08458476901c80dbfa8e9d1b6b7
Kim G <xiaole.guo@yahoo.com>
2022-06-03 22:28:07 -0600

    Ran rubocop

--- app/services/users_service.rb
+++ app/services/users_service.rb
@@ -1,23 +1,22 @@
 class UsersService
-
   def self.find_or_create_user(params)
-  	{
-  	  data: {
-  	    type: "user",
-  	    id: "1",
-  	    attributes: {
-  	      name: params[:name],
-  	      email: params[:email],
-  	      address: nil
-  	    }
-  	  }
-  	}
+    {
+      data: {
+        type: 'user',
+        id: '1',
+        attributes: {
+          name: params[:name],
+          email: params[:email],
+          address: nil
+        }
+      }
+    }
 
-  	# response = conn('users').post('post') do |req|
-  	#   #req.headers['mmim_api_key'] = 'tbd'
-  	#   req.body = params.to_json
-  	# end
-   #  JSON.parse(response.body, symbolize_names: true) 
+    # response = conn('users').post('post') do |req|
+    #   #req.headers['mmim_api_key'] = 'tbd'
+    #   req.body = params.to_json
+    # end
+    #  JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.conn(url)
