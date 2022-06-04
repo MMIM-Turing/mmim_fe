34b9bbd2bd6a1a444ed18466887a1b6ca178fc9f
Kim G <xiaole.guo@yahoo.com>
2022-06-03 22:15:59 -0600

    Configure omniauth mock

--- app/services/users_service.rb
+++ app/services/users_service.rb
@@ -1,11 +1,23 @@
 class UsersService
 
   def self.find_or_create_user(params)
-  	response = conn('users').post('post') do |req|
-  	  #req.headers['mmim_api_key'] = 'tbd'
-  	  req.body = params.to_json
-  	end
-    JSON.parse(response.body, symbolize_names: true) 
+  	{
+  	  data: {
+  	    type: "user",
+  	    id: "1",
+  	    attributes: {
+  	      name: params[:name],
+  	      email: params[:email],
+  	      address: nil
+  	    }
+  	  }
+  	}
+
+  	# response = conn('users').post('post') do |req|
+  	#   #req.headers['mmim_api_key'] = 'tbd'
+  	#   req.body = params.to_json
+  	# end
+   #  JSON.parse(response.body, symbolize_names: true) 
   end
 
   def self.conn(url)
