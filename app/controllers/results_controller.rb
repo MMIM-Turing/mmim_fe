968271deffb03c6c41393bb0cd9531a40668330f
Will Medders <wmedders@gmail.com>
2022-06-04 12:51:00 -0400

    Feature Test: results index multiple tests

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,5 +1,6 @@
 class ResultsController < ApplicationController
   before_action :require_address
+  before_action :default_category
 
   def index
     # @locations = LocationsFacade.top_5(search_params)
@@ -16,6 +17,13 @@ class ResultsController < ApplicationController
     if search_params[:address_1].empty? || search_params[:address_2].empty?
       flash[:alert] = "Please fill out both address fields"
       redirect_to '/'
+    else
+    end
+  end
+
+  def default_category
+    if search_params[:category].empty?
+      search_params[:category] = "coffee shop"
     else
     end
   end
---
+++ spec/features/results/index_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe 'results page' do
+  before :each do
+    visit '/'
+    fill_in :address_1, with: "123 st, city, state, 50001"
+    fill_in :address_2, with: "124 st, city, state, 50003"
+  end
+  describe 'map card' do
+    xit 'displays a map' do
+      click_button 'Search'
+
+      expect(page).to have_xpath("//a")
+    end
+  end
+
+  describe 'result list card' do
+    context 'category' do
+      it 'displays category' do
+        fill_in :category, with: "gym"
+        click_button 'Search'
+
+        expect(page).to have_content("gym")
+        expect(page).to have_no_content("coffee shop")
+      end
+
+      it 'displays coffee shop as the default category' do
+        click_button 'Search'
+        #note that we didn't fill out :category in the before block
+        expect(page).to have_content("coffee shop")
+      end
+
+
+      it 'has a form to update category' do
+        click_button 'Search'
+        fill_in :category, with: "bar"
+        click_button 'Update Category'
+
+        expect(current_path).to eq('/results')
+        expect(page).to have_content("bar")
+        expect(page).to have_no_content("coffee shop")
+      end
+    end
+  end
+end
