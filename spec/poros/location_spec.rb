9767f89161d63ec20ea724d19dd5e16354387fbf
Will Medders <wmedders@gmail.com>
2022-06-04 19:50:50 -0400

    Poro Location #exists

---
+++ spec/poros/location_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Location do
+  it 'exists' do
+    attributes = {
+                  name: "Ben & Jerry's",
+                  place_id: "XXX1",
+                  address: "123 st, city, state, 50001",
+                  longitude: "35.04443068881643",
+                  latitude: "-85.30914752416172",
+                  rating: "4.1",
+                  image_url: "https://iili.io/XDFtwX.jpg",
+                  price_rating: "$"
+
+    }
+
+    location = Location.new(attributes)
+
+    expect(location).to be_a Location
+    expect(location.name).to be_a String
+    expect(location.place_id).to be_a String
+    expect(location.address).to be_a String
+    expect(location.longitude).to be_a String
+    expect(location.latitude).to be_a String
+    expect(location.rating).to be_a String
+    expect(location.image_url).to be_a String
+    expect(location.price_rating).to be_a String
+  end
+end
