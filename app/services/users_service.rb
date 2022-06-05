a3e0cf581df348a1d3797d422a75b12fde45998b
Kim G <xiaole.guo@yahoo.com>
2022-06-05 11:36:48 -0600

    wip

--- app/services/users_service.rb
+++ app/services/users_service.rb
@@ -1,36 +1,15 @@
 class UsersService
   def self.find_or_create_user(params)
-    # {
-    #   data: {
-    #     type: 'user',
-    #     id: '1',
-    #     attributes: {
-    #       name: params[:name],
-    #       email: params[:email],
-    #       address: params[:address]
-    #     }
-    #   }
-    # }
-
     response = conn("users?params=#{params.to_json}").post
-     JSON.parse(response.body, symbolize_names: true)
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.create_or_update_address(params)
-        {
-      data: {
-        type: 'user',
-        id: '1',
-        attributes: {
-          name: params[:name],
-          email: params[:email],
-          address: params[:address]
-        }
-      }
-    }
+    response = conn("users?params=#{params.to_json}").put
+    JSON.parse(response.body, symbolize_names: true)
   end
 
-  def self.conn(url)
-    Faraday.new("http://localhost:3000/api/v1/#{url}")
+  def self.conn(uri)
+    Faraday.new("http://localhost:3000/api/v1/#{uri}")
   end
 end
