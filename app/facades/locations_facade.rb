842a2fc321e4d37f54bdf3cba8c4374bef005291
Kim G <xiaole.guo@yahoo.com>
2022-06-05 23:26:50 -0600

    handle no results found edge case

--- app/facades/locations_facade.rb
+++ app/facades/locations_facade.rb
@@ -2,6 +2,7 @@ class LocationsFacade
   def self.address_location_search(params)
 
     data = BackendService.get_locations(params)[:data]
+    require 'pry'; binding.pry
     list = data.map do |data|
       Location.new(data[:attributes])
     end
@@ -11,7 +12,11 @@ class LocationsFacade
     coordinates = locations.map do |location|
       [location.name, location.latitude, location.longitude]
     end
-    average = ['average', coordinates.map{ |c| c[1]}.sum/coordinates.count, coordinates.map{ |c| c[2]}.sum/coordinates.count]
-    coordinates.unshift(average)
+    if coordinates != []
+      average = ['average', coordinates.map{ |c| c[1]}.sum/coordinates.count, coordinates.map{ |c| c[2]}.sum/coordinates.count]
+      coordinates.unshift(average)
+    else 
+      'No results found'
+    end
   end
 end
