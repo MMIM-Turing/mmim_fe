0fd23a9beac34781bf6aa4acc59a2517c09ae789
Kim G <xiaole.guo@yahoo.com>
2022-06-05 22:48:33 -0600

    fix price_rating rendering issue

--- app/poros/location.rb
+++ app/poros/location.rb
@@ -16,6 +16,14 @@ class Location
     @latitude = attributes[:lat]
     @rating = attributes[:rating]
     @image_url = attributes[:image_url]
-    @price_rating = attributes[:price_rating]
+    @price_rating = price_conv(attributes[:price_level]) 
+  end
+
+  def price_conv(level=nil)
+    return '$' if level == 1
+    return '$$' if level == 2
+    return '$$$' if level == 3
+    return '$$$$' if level == 4
+    return 'N/a' if level==nil 
   end
 end
