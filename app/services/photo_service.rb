1d1ffa4de9f6ab2a1b71fe09f2cbfb88ac539ff6
Kim G <xiaole.guo@yahoo.com>
2022-06-07 15:31:19 -0600

    Styling all results page to match a format

--- app/services/photo_service.rb
+++ app/services/photo_service.rb
@@ -1,12 +1,10 @@
 class PhotoService
   def self.get_url(photo_reference)
   	response = Faraday.get("https://maps.googleapis.com/maps/api/place/photo") do |faraday|
-  	     faraday.params['maxwidth'] = 400
+  	     faraday.params['maxwidth'] = 200
   	     faraday.params['photo_reference'] = photo_reference
   	     faraday.params['key'] = ENV.fetch('google_maps_api_key', nil)
   	end
   	response.headers[:location]
   end
-
-
 end
