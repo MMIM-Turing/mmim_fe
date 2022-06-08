7cac2414296a1a56fbcc64b8a318bec17a325b9c
Kim G <xiaole.guo@yahoo.com>
2022-06-07 19:53:19 -0600

    finish search_by_email testing

--- spec/features/dashboard/user_results/index_spec.rb
+++ spec/features/dashboard/user_results/index_spec.rb
@@ -10,6 +10,19 @@ RSpec.describe 'results page' do
     allow(PhotoService).to receive(:get_url).and_return('http//url')
   end
 
+
+  
+  it 'searches by existing user email- happy path', :vcr do 
+    within "#user_search" do
+      fill_in :address_1, with: "2300 Steele St, Denver, CO 80205"
+      fill_in :user_b_email, with: "email@email.com"
+      click_button 'Search'
+    end 
+
+    expect(current_path).to eq('/dashboard/user_results')
+  end
+
+
   it 'searches by existing user email- sad path', :vcr do 
     within "#user_search" do
       fill_in :address_1, with: "2300 Steele St, Denver, CO 80205"
@@ -19,6 +32,18 @@ RSpec.describe 'results page' do
     end 
       expect(current_path).to eq(dashboard_path)
       expect(page).to have_content('Invalid user email. Please try again!')
+  end
+
+  it 'searches by existing user email - no default add', :vcr do 
+    within "#user_search" do
+      fill_in :address_1, with: "2300 Steele St, Denver, CO 80205"
+      fill_in :user_b_email, with: "no_add@email.com"
+      fill_in :category, with: "gym"
+      click_button 'Search'
+    end 
+
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content('no_add@email.com has not set a default address, please search by address instead!')
   end
 end
   
