1bae6691bb4888c4a12a173111096c0bf24fddfc
Kim G <xiaole.guo@yahoo.com>
2022-06-09 08:24:26 -0600

    pending meeting disappears once meeting is accepted

--- app/poros/suggested_meeting.rb
+++ app/poros/suggested_meeting.rb
@@ -1,8 +1,9 @@
 class SuggestedMeeting
-  attr_reader :host_email, :guest_email, :locations
+  attr_reader :host_email, :guest_email, :locations, :place_ids
   def initialize(hash)
     @host_email = hash[:host_email]
     @guest_email = hash[:guest_email]
+    @place_ids = hash[:locations].map {|loc| loc.place_id}
     @locations = hash[:locations]
   end
 end
