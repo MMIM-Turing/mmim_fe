2302545f57886c495e5caa9ed54df6e6bbe4334f
Kim G <xiaole.guo@yahoo.com>
2022-06-08 22:33:33 -0600

    reformat photo_Service

--- app/services/photo_service.rb
+++ app/services/photo_service.rb
@@ -1,10 +1,11 @@
 class PhotoService
   def self.get_url(photo_reference)
-  	response = Faraday.get("https://maps.googleapis.com/maps/api/place/photo") do |faraday|
-  	     faraday.params['maxwidth'] = 200
-  	     faraday.params['photo_reference'] = photo_reference
-  	     faraday.params['key'] = ENV.fetch('google_maps_api_key', nil)
-  	end
+  	response = Faraday.get("https://maps.googleapis.com/maps/api/place/photo?maxwidth=200&photo_reference=#{photo_reference}&key=#{ENV['google_maps_api_key']}") 
+   #  response = Faraday.get("https://maps.googleapis.com/maps/api/place/photo") do |faraday|
+  	#      faraday.params['maxwidth'] = 200
+  	#      faraday.params['photo_reference'] = photo_reference
+  	#      faraday.params['key'] = ENV.fetch('google_maps_api_key', nil)
+  	# end
   	response.headers[:location]
   end
 end
