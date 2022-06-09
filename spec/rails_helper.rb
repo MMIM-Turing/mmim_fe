ebb633b7a84ea6fd849465fd33b539229fe3f188
Kim G <xiaole.guo@yahoo.com>
2022-06-09 13:46:14 -0600

    reconfigure vcr

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -90,5 +90,5 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.configure_rspec_metadata!
   config.filter_sensitive_data('DONT SHOW MY API KEY') { ENV['govt_api_key'] }
-  config.default_cassette_options = { re_record_interval: 1.day }
+  config.default_cassette_options = { :record => :new_episodes }
 end
