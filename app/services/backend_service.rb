b040b3d65fe1e9ba7e1aad7a8b6f0684615c4ba0
Kim G <xiaole.guo@yahoo.com>
2022-06-09 10:49:13 -0600

    move conn to private method

--- app/services/backend_service.rb
+++ app/services/backend_service.rb
@@ -1,34 +1,39 @@
 class BackendService
-  def self.conn
-    if Rails.env == 'development' || Rails.env == 'test'
-      Faraday.new(ENV["backend_server_dev"])
-    else
-      Faraday.new(ENV["backend_server_pro"])
-    end
+  class << self
+  
 
-  end
-
-  def self.get_locations(params)
+  def get_locations(params)
     response = conn.get("search?params=#{params.to_json}")
     JSON.parse(response.body, symbolize_names: true)
   end
 
 
-  def self.get_locations_by_user(params)
+  def get_locations_by_user(params)
     p = params.to_json.gsub("@", "%40")
     response = conn.get("search_by_user?params=#{p}")
     JSON.parse(response.body, symbolize_names: true)
   end
-  def self.create_meetings(params)
+  def create_meetings(params)
     p = params.to_json.gsub("@", "%40")
     p = p.gsub("#", '%23')
     response = conn.post("meeting?params=#{p}")
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def self.get_meetings(params)
+  def get_meetings(params)
     p = params.to_json.gsub("@", "%40")
     response = conn.get("user_meetings?params=#{p}")
     JSON.parse(response.body, symbolize_names: true)
+  end
+
+private 
+  def conn
+      if Rails.env == 'development' || Rails.env == 'test'
+        Faraday.new(ENV["backend_server_dev"])
+      else
+        Faraday.new(ENV["backend_server_pro"])
+      end
+
+    end
   end
 end
--- spec/facades/meetings_facade_spec.rb
+++ spec/facades/meetings_facade_spec.rb
@@ -1,18 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe MeetingsFacade do
-  xit 'creates suggested meeting' do
-    data = JSON.parse(File.read('spec/fixtures/suggested_meetings_data.json'), symbolize_names: true)
-    
-
-    meeeting = MeetingsFacade.suggested_meeting(data)
-
-    expect(meeting).to be_a SuggestedMeeting
-    expect(meeting.guest_email).to eq("user_search@email.com")
-    expect(meeting.host_email).to eq("user@gmail.com")
-    expect(meeting.locations).to be_all Locatioins
-  end
-
   it 'creates a meeting', :vcr do 
     params = {:host_email=>"kimmieguo@gmail.com", :guest_email=>"xiaole.guo1@gmail.com", :location_name=>"Lucile's Creole Cafe", :location_address=>"2852 West Bowles Avenue, Littleton"}
     results = MeetingsFacade.create_meeting(params)
